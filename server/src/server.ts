f2d6b75eda919b03a461a2ea612ed45a5a79f978
Aditya Jha <141293751+ad-itya07@users.noreply.github.com>
2026-07-27 14:44:36 +0530

    backend initialisation and database setup

---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+    res.send("Server is up and running!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+});
