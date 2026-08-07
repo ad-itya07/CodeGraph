6656074bc477c65f4bff44aa138eb799cbf22811
Aditya Jha <141293751+ad-itya07@users.noreply.github.com>
2026-08-08 00:32:20 +0530

    Feat: Implemented 2 stages of Repo ingestion. 1. Filtering out supported files. 2. Parsing files and making ASTs

---
+++ server/src/parser/index.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import { getSupportedRepositoryFiles } from "./walker/repositoryWalker.js";
+import { NoSupportedFileError } from "@/errors/NoSupportedFileError.js";
+import { parseFile } from "./babel/parseFile.js";
+import { ParsedFile } from "./models/ParsedFile.js";
+import { ParseFailure } from "./models/ParseFailure.js";
+import { ParsedRepository } from "./models/ParsedRepository.js";
+
+/*
+* TODO: later call this parse function inside the worker
+* const parser = new Parser();
+* await parser.parse(repositoryPath);
+*/
+export class Parser {
+    async parse(repositoryPath: string): Promise<ParsedRepository> {
+        // --- 1. Filtering out Supported Files ---
+        const supportedFiles = getSupportedRepositoryFiles(repositoryPath);
+
+        if (supportedFiles.length === 0) {
+            throw new NoSupportedFileError();
+        }
+
+        // --- 2. Parsing files and making ASTs ---
+        const parsedFiles: ParsedFile[] = [];
+        const failedFiles: ParseFailure[] = [];
+        for (const file of supportedFiles) {
+            try {
+                console.log(`Parsing ${file}`);
+                parsedFiles.push(parseFile(file));
+            } catch (err: unknown) {
+                console.error(`Failed to parse file ${file}`);
+                if (err instanceof Error) {
+                    failedFiles.push({ filePath: file, message: err.message, cause: err });
+                } else {
+                    failedFiles.push({ filePath: file, message: "Unknown parsing error", cause: err });
+                }
+            }
+        }
+
+        return { repositoryPath, files: parsedFiles, failures: failedFiles, relationships: [] };
+    }
+}
+
+const parser = new Parser();
+const result = await parser.parse(path.join(process.cwd(), "uploads", "demo"));
+console.log(result);
