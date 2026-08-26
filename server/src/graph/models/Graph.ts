31b390756a8686201b6512ec6fd4d9a54fa27eaf
Aditya Jha <141293751+ad-itya07@users.noreply.github.com>
2026-08-26 11:40:05 +0530

    refactor: add comments to clarify Graph interface properties

--- server/src/graph/models/Graph.ts
+++ server/src/graph/models/Graph.ts
@@ -2,11 +2,11 @@ import { GraphNode, GraphNodeKind } from "./GraphNode.js";
 import { GraphEdge } from "./GraphEdge.js";
 
 export interface Graph {
-    nodes: Map<string, GraphNode>;
-    edges: Map<string, GraphEdge>;
+    nodes: Map<string, GraphNode>; // NodeId -> Node Object
+    edges: Map<string, GraphEdge>; // EdgeId -> Edge Object
 
-    nodesByKind: Map<GraphNodeKind, Set<string>>;
+    nodesByKind: Map<GraphNodeKind, Set<string>>; // NodeKind -> Set of NodeIds
 
-    outgoingEdges: Map<string, Set<string>>;
-    incomingEdges: Map<string, Set<string>>;
+    outgoingEdges: Map<string, Set<string>>; // source NodeId -> Set of Outgoing EdgeIds
+    incomingEdges: Map<string, Set<string>>; // target NodeId -> Set of Incoming EdgeIds
 }
